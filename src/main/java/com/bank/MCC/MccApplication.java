fb5e69724405fa46eb3885acf176a49c79c88d17
yigitsahin <yigit.sahin@denk-it.com>
2022-01-28 17:30:31 +0300

    First Commit, Github is created. Project created from Spring Initilizer

---
+++ src/main/java/com/bank/MCC/MccApplication.java
@@ -0,0 +1,13 @@
+package com.bank.MCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccApplication.class, args);
+	}
+
+}
