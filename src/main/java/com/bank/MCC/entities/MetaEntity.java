2e959b4af553474f8bc6b02bd09419dd061eace3
yigitsahin <yigit.sahin@denk-it.com>
2022-01-28 18:34:39 +0300

    Meta Entites are created

--- src/main/java/com/bank/MCC/entities/MetaEntity.java
+++ src/main/java/com/bank/MCC/entities/MetaEntity.java
@@ -1,4 +1,42 @@
 package com.bank.MCC.entities;
 
-public class MetaEntity {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="meta")
+public class MetaEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    Integer id;
+
+    @Column(name = "nameOfApplication")
+    String nameOfApplication;
+
+    @Column(name = "ownerOfApplication")
+    String ownerOfApplication;
+
+    @Column(name = "configManagerOfApplication")
+    String configManagerOfApplication;
+
+    @Column(name = "version")
+    int version;
+
+    @Column(name = "createdDate")
+    private Date createdDate;
+
+    @Column(name = "updateDate")
+    private Date updateDate;
+
 }
--- src/main/java/com/bank/MCC/entities/MetaOldEntity.java
+++ src/main/java/com/bank/MCC/entities/MetaOldEntity.java
@@ -1,4 +1,45 @@
 package com.bank.MCC.entities;
 
-public class MetaOldEntity {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="meta_old")
+public class MetaOldEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    Integer id;
+
+    @Column(name = "nameOfApplication")
+    String nameOfApplication;
+
+    @Column(name = "ownerOfApplication")
+    String ownerOfApplication;
+
+    @Column(name = "configManagerOfApplication")
+    String configManagerOfApplication;
+
+    @Column(name = "version")
+    int version;
+
+    @Column(name = "createdDate")
+    private Date createdDate;
+
+    @Column(name = "updateDate")
+    private Date updateDate;
+
+    @ManyToOne
+    @JoinColumn(name = "meta_id")
+    private MetaEntity metaId;
+
 }
